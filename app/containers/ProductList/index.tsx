0485da8ea1d9e226f856072b905a884112fb1712
Duy Hoc <duyhocnguyen97@gmail.com>
2020-10-02 22:40:17 +0700

    Test

--- app/containers/ProductList/index.tsx
+++ app/containers/ProductList/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import 
 
 //Khởi tạo 1 kiểu dữ liệu mới, là kiểu dữ liệu của props sẽ được truyền vào
 interface ProductListProps{}
 
 export const ProductList:React.FC<ProductListProps>=(props)=>{
-    return <div>ProductList</div>
+
+	const [data,setData]=useState(product)
+    return <div>
+	<div>ProductList</div>
+		{data.map((item,index)=>{
+			return <ProductItem/>
+		}}
+	</div>
 }
--- app/utils/data.ts
+++ app/utils/data.ts
@@ -1,2 +1,33 @@
 //Tạo data giả cho product
-const tempData = []
+const tempData = 
+[
+    {
+        "id": 1,
+        "image": "https://fullstackvietnam.com/ngcms_assets/uploads/2018/12/maxresdefault-700x394.jpg",
+        "name": "san pham 1",
+        "price" : 1000   
+    },
+    {
+        "id":2,
+        "image": "https://fullstackvietnam.com/ngcms_assets/uploads/2018/12/Screen-Shot-2018-12-02-at-19.22.14-700x394.png",
+        "name": "san pham 2",
+        "price" : 2000
+    },
+    {
+        "id":3,
+        "image": "https://fullstackvietnam.com/ngcms_assets/uploads/2019/01/792484_cc98_3-700x394.jpg",
+        "name": "san pham 3",
+        "price" : 3000
+    },
+    {
+        "id" : 4,
+        "image" : "https://fullstackvietnam.com/ngcms_assets/uploads/2018/08/background_img_01b-700x394.jpg",
+        "name": "san pham 4",
+        "price" : 40000
+    }
+
+
+
+]
+
+export {tempData]
